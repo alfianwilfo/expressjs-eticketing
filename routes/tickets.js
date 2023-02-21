11c5c5ae8b40cb826c78132aab96fd67c52b1b6d
Alfian Horis <alfian.horis@merahciptamedia.co.id>
2023-02-21 14:27:41 +0700

    middleware validatorjs for input create new ticket done

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 let controller = require("../controllers/ticketController.js")
+let { validateInput } = require("../middlewares/validator")
 
 router.get("/", controller.getAllTicket)
-.post('/', controller.createTicket)
+.post('/', validateInput, controller.createTicket)
 .patch("/:id", controller.updateStatusTicket)
 
 module.exports = router;
