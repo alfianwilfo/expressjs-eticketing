a1c97b462fad654bcb4e98cfbc04b88c5c6716d0
Alfian Horis <alfian.horis@merahciptamedia.co.id>
2023-02-23 16:31:07 +0700

    add new middleware

--- routes/departement.js
+++ routes/departement.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 let controller = require("../controllers/departementController")
+let authentication = require("../middlewares/authentication")
 
-router.get("/", controller.getAllDepartement)
+router.get("/", authentication, controller.getAllDepartement)
 
 
 module.exports = router
