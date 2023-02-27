8b9b633336e84733b671bde042769536a22c4d91
Alfian Horis <alfian.horis@merahciptamedia.co.id>
2023-02-27 08:26:32 +0700

    new commit

--- controllers/departementController.js
+++ controllers/departementController.js
@@ -10,3 +10,4 @@ class DepartementController {
     }
 }
 module.exports = DepartementController
+
--- routes/tickets.js
+++ routes/tickets.js
@@ -4,7 +4,8 @@ let controller = require("../controllers/ticketController.js")
 let { validateInputCreate, validatorPatchInput } = require("../middlewares/validator")
 let authen = require("../middlewares/authentication")
 
-router.get("/", authen, controller.getAllTicket)
+router.get("/", controller.getAllTicket)
+.get("/my-request", authen, controller.getAllMyRequest)
 .post('/', authen, validateInputCreate, controller.createTicket)
 .patch("/:id", validatorPatchInput, controller.updateStatusTicket)
 
